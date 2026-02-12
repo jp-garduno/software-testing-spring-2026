ef94b42bd41446f279948e0572f707f6c852b077
Serch-Espinosa <checochique@gmail.com>
2026-02-11 21:36:50 -0600

    fix: modify hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,9 +2,13 @@
 Docstring for hello_world
 A simple hello world module.
 """
+
+
 def hello_world():
     """
     Docstring for hello_world
     """
-    print("Hola mundo!")
+    return "Hola mundo!"
+
+
 hello_world()
