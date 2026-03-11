6236dee4fc86fb77c0691f3bac852b05f9d6bec8
Juan Pablo Garduno <juan.p.garduno.calderon@intel.com>
2026-03-11 11:56:02 -0600

    chore: add tests for fetch data from api
    Add unit tests using a mock for the fetch_data_from_api function.

--- white_box/test_mockup_exercises.py
+++ white_box/test_mockup_exercises.py
@@ -6,7 +6,47 @@ Mock up testing examples.
 import unittest
 from unittest.mock import patch
 
-from white_box.mockup_exercises import perform_action_based_on_time
+from white_box.mockup_exercises import fetch_data_from_api, perform_action_based_on_time
+
+
+class TestFetchDataFromApi(unittest.TestCase):
+    """
+    Fetch data from API unittest class.
+    """
+
+    @patch("white_box.mockup_exercises.requests.get")
+    def test_fetch_data_from_api_success(self, mock_get):
+        """
+        Success case.
+        """
+        # Set up the mock response
+        mock_get.return_value.json.return_value = {"key": "value"}
+
+        # Mock the requests.get method
+        # with patch("requests.get") as mock_get:
+        #     mock_get.return_value.status_code = 200
+        #     mock_get.return_value.json.return_value = [
+        #         {"id": 1, "title": "Title 1", "body": "Body 1"},
+        #         {"id": 2, "title": "Title 2", "body": "Body 2"},
+        #     ]
+
+        # patch_get = patch('requests.get')
+        # mock_get = patch_get.start()
+        # mock_get.return_value.status_code = 200
+        # mock_get.return_value.json.return_value = [
+        #     {"id": 1, "title": "Title 1", "body": "Body 1"},
+        #     {"id": 2, "title": "Title 2", "body": "Body 2"},
+        # ]
+        # patch_get.stop()
+
+        # Call the function under test
+        result = fetch_data_from_api("https://api.example.com/data")
+
+        # Assert that the function returns the expected result
+        self.assertEqual(result, {"key": "value"})
+
+        # Assert that requests.get was called with the correct URL
+        mock_get.assert_called_once_with("https://api.example.com/data", timeout=10)
 
 
 class TestPerformActionBasedOnTime(unittest.TestCase):
